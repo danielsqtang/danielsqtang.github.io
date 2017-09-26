c03ed666f86db7b4d869c1c6fe6533c12473cdb8
Daniel Tang <danielsqtang@gmail.com>
2017-09-26 14:48:30 -0400

    Create cardDeck.java

---
+++ cardDeck.java
@@ -0,0 +1,10 @@
+//52-card set
+
+public enum Suit {
+  Club (0), Diamond (1), Heart (2), Spade (3);
+  private int value;
+  private Suit(int v) { value = v; }
+  public int getValue() { return value; }
+  public static Suit getSuitFromValue(int value) {  }
+}
+
